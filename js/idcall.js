2aa71aaa9bc0f63b78eeec8642c973bc4a9dd6b4
msdharani1 <msdharani1.msdharaniofficial@gmail.com>
2023-10-25 11:34:40 +0530

    solved the service link

---
+++ js/idcall.js
@@ -0,0 +1,7 @@
+var page = document.getElementById('product_call');
+var mainpage = document.getElementById('hide-body');
+
+function idcall(){
+    page.style.display = "block";
+    mainpage.style.display = "none";
+}
